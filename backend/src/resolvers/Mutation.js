5d058a2e900d1049e4aad0830c0405c8582c9560
Suraj Bahadur <surajbahadur58@gmail.com>
2020-11-01 13:04:08 +0530

    graphql and prisma is added next is appolo client

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,20 @@
-const Mutations = {};
+const { compareSync } = require("bcryptjs");
+
+const Mutations = {
+     async createItem(parent, args, ctx, info) {
+
+        const item =  await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+
+        console.log(item)
+
+        return item;
+
+     }
+
+};
 
 module.exports = Mutations;
