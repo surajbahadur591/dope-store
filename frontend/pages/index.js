10368753757859976c39090019a7d5f21357e1d4
Suraj Bahadur <surajbahadur58@gmail.com>
2020-11-11 10:45:32 +0530

    comment

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,12 @@
 import Link from 'next/link'
 import { render } from 'nprogress'
 import Items from '../components/Items'
-
+// comment
 const Home = props => (
         <div>
             <Items page={parseFloat(props.query.page) || 1}/>
         </div>
 
-);
+); 
 
 export default Home
